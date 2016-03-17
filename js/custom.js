f3c69d1507b3380de83eb8d1acc936adc1052a76
uhassist <assist@hawaii.edu>
2016-03-17 13:13:30 -1000

    custom.js -Added check and alert for missing Location name field -Added check and alert for missing OPAC Display Name field

--- js/custom.js
+++ js/custom.js
@@ -8,17 +8,29 @@ function checkForm() {
 
   console.log($('#locationcode').val().length);
 
-  var locationcodelength = $('#locationcode').val().length;
+  var locationcodelen = $('#locationcode').val().length;
 
-  if(locationcodelength == 0) {
-  	msg += "Location Code required\n"
+  if(locationcodelen == 0) {
+  	msg += "Location Code required\n";
   }
   
   var whiteSpace = $('#locationcode').val().indexOf(' ')>=0;
   if(whiteSpace) {
-  	msg += "Your Location Code has 1 or more spaces\n"
+  	msg += "Your Location Code has 1 or more spaces\n";
+  }
+
+  var locationnamelen = $('#locationname').val().length;
+
+  if(locationnamelen == 0) {
+  	msg += "Location Name required\n";
   }
   
+  var opacdisplaynamelen = $('#opacdisplayname').val().length;
+
+  if(opacdisplaynamelen == 0) {
+  	msg += "OPAC Display Name required\n";
+  }
+
   if(msg!="") {
   	alert(msg);
   }
